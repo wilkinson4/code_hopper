18c1b5438c71dcf08ea2e31cd069fc40560e7efe
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-02-02 11:00:14 -0600

    added FinalBossMoth png and cs file

---
+++ New Unity Project/Assets/Bugs/FinalBossMoth.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBossMoth: MonoBehaviour
+{
+    public float speed = 1.5f;
+    public float rotateSpeed = 5.0f;
+    public float rangex = 10f;
+    public float rangey = 4.5f;
+    private bool notDead = true;
+    private bool locked = false;
+    public Sprite deadimage;
+    public static bool FlyOn = true;
+
+
+    Vector3 newPosition;
+
+    void Start()
+    {
+        PositionChange();
+    }
+
+    void PositionChange()
+    {
+        newPosition = new Vector2(Random.Range(-rangex, rangex), Random.Range(-rangey, rangey));
+    }
+
+    void Update()
+    {
+        if (notDead)
+        {
+            if (Vector2.Distance(transform.position, newPosition) < 1)
+                PositionChange();
+
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed);
+
+            LookAt2D(newPosition);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * .1f);
+            SpinToEnd2D(newPosition);
+        }
+    }
+
+    void LookAt2D(Vector3 lookAtPosition)
+    {
+        float distanceX = lookAtPosition.x - transform.position.x;
+        float distanceY = lookAtPosition.y - transform.position.y;
+        float angle = Mathf.Atan2(distanceX, distanceY) * Mathf.Rad2Deg;
+
+        Quaternion endRotation = Quaternion.AngleAxis(angle, Vector3.back);
+        transform.rotation = Quaternion.Slerp(transform.rotation, endRotation, Time.deltaTime * rotateSpeed);
+    }
+    void SpinToEnd2D(Vector3 lookAtPosition)
+    {
+
+        transform.Rotate(Vector3.forward * -5);
+    }
+    private void OnMouseDown()
+    {
+        if (!locked)
+        {
+            XSpawn.currentflycount--;
+            locked = true;
+            this.GetComponent<SpriteRenderer>().sprite = deadimage;
+            notDead = false;
+            newPosition = new Vector2(transform.position.x, -30f);
+        }
+    }
+}
+
+
+
+
