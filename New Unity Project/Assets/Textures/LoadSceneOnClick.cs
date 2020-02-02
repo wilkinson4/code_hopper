c3b9e4620cfb6a6cd874582e52aa39b90b6e3701
Bobby Brady <bbrady91@gmail.com>
2020-02-02 11:54:11 -0600

    got the play button to work

---
+++ New Unity Project/Assets/Textures/LoadSceneOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+	//public bool isStart;
+
+    public void OnMouseUp()
+	{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		
+	}
+}
